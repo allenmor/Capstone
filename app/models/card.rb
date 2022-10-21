d15dd3e0664294e56dcd642cbe3f55c98057d54b
Allen Mordukhaev <allenmor93@gmail.com>
2022-10-21 11:20:54 -0400

    again

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,7 @@
 class Card < ApplicationRecord
     belongs_to :user
-    # validates :name, presence: true
-    # validates :number, length: { is: 16 }, presence: true
-    # validates :exp, length: { is: 4 }
-    # validates :code, length: { is: 3 }
+    validates :name, presence: true
+    validates :number, length: { is: 16 }, presence: true
+    validates :exp, length: { is: 4 }
+    validates :code, length: { is: 3 }
 end
